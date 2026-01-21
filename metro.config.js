e5b82242b2e37660dc9b1139efc09c02b8c06b3b
Serhat Morkoç <serhatmorkoc@atlasyazilim.com.tr>
2026-01-21 11:18:19 +0300

    --comm

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure Relay artifacts are properly resolved
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
 module.exports = config;
